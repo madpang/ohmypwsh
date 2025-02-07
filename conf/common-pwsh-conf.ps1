eaf6414d0f638f22476af27a0ac0b119f7cdf087
madpang <133752527+madpang@users.noreply.github.com>
2025-02-08 00:47:34 +0900

    Create a bare-bone powershell profile
    - add profile loading logic
    - add locale setting
    - unload PSReadline (will be re-loaded after further configuration)
    - add environment variable for GPG

---
+++ conf/common-pwsh-conf.ps1
@@ -0,0 +1,30 @@
+<#
+	@brief: Common PowerShell configuration for all platforms
+
+	@detaisl:
+	- Locale settings
+	- Environment setup
+	- PSReadline configuration
+	- prompt configuration
+
+	@author:
+	- madpang
+
+	@date:
+	- created on 2021-08-05
+	- updated on 2024-03-14
+#>
+
+# /// Locale settings
+[cultureinfo]::CurrentCulture = 'ja-JP'
+
+# /// Environment setup
+# ////////////////////////////////////////////////////////////
+
+# Unload PSReadline (@note, ince PSReadline is automatically loaded, we unload it first to do some configuration)
+Remove-Module PSReadline
+
+# Set environment variables
+Set-Item -Path "env:GPG_TTY" -Value "$(tty)" # @note, use `Get-Command -Name tty` to check whether the command is available
+
+# /// PSReadline configuration
