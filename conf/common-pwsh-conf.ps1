5a348d6542f22c7496bf08276cf1194bbe8a2421
madpang <133752527+madpang@users.noreply.github.com>
2025-02-08 11:03:41 +0900

    Implementing platform-dependent Switch-Theme function
    including PSReadLine configuration inside Switch-Theme

--- conf/common-pwsh-conf.ps1
+++ conf/common-pwsh-conf.ps1
@@ -13,7 +13,7 @@
 
 	@date:
 	- created on 2021-08-05
-	- updated on 2024-11-17
+	- updated on 2024-11-23
 
 	@todo:
 	- [x] Rearrange prompt function
@@ -39,32 +39,52 @@ Set-Variable -Name "Global:PD_PROMPT_MACHINE" -Value ([Environment]::MachineName
 
 Set-Variable -Name "Global:PD_PROMPT_USER" -Value ([Environment]::UserName) -Visibility Private
 
-Set-Variable `
-	-Name "Global:PD_COLOR_PALLETE" `
+New-Variable `
+	-Name "PD_COLOR_PALLETE" `
 	-Value @{
 		'Licorice'   = '#000000';
 		'Lead'       = '#212121';
+		'Tin'        = '#919191'
+		'Silver'     = '#D6D6D6';
 		'Snow'       = '#FFFFFF';
 		'Cayenne'    = '#941100';
 		'Ocean'      = '#005493';
+		'Midnight'   = '#011993';
+		'Lemon'      = '#FFFB00'
 		'Banana'     = '#FFFC79';
 		'Salmon'     = '#FF7E79';
 		'Spindrift'  = '#73FCD6';
 		'Sky'        = '#76D6FF';
-		'Silver'     = '#D6D6D6';
 		'Strawberry' = '#FF2F92';
 	} `
+	-Scope Global `
+	-Visibility Public `
+	-Option ReadOnly
+
+New-Variable `
+	-Name "PD_PROMPT_COLOR" `
+	-Value @{
+		'BG_USER'    = '255;255;255';
+		'FG_USER'    = '0;0;0';
+		'BG_MACHINE' = '255;255;255';
+		'FG_MACHINE' = '0;0;0';
+		'BG_PATH'    = '255;255;255';
+		'FG_PATH'    = '0;0;0';
+		'BG_AUX'     = '255;255;255';
+		'FG_AUX'     = '0;0;0';
+	} `
+	-Scope Global `
 	-Visibility Private
 
 # Set-Item -Path "variable:$Global:PD_ERROR_STAT" -Value $true
 
 # /////////////////// Utility functions ////////////////////////
 
-# Function to convert #RRGGBB to ANSI escape sequence
-Function Convert-HexColorToANSI {
+# @brief: convert #RRGGBB to ANSI escape sequence
+function Convert-HexColorToANSI {
 	[OutputType([String])]
 	param (
-		[Parameter(Mandatory = $True, Position = 0)][string]$hex_color # The color in #RRGGBB format
+		[Parameter(Mandatory = $True, Position = 0)][string]$hex_color
 	)
 
 	# Validate input format
@@ -80,68 +100,10 @@ Function Convert-HexColorToANSI {
 	return "${r};${g};${b}"
 }
 
-# Function to get light/dark theme
-# out 
-Function Get-Theme {
-	[OutputType([Hashtable])]
-	param (
-		[Parameter(Mandatory = $True, Position = 0)][string]$theme # The theme name
-	)
-
-	# Validate input format
-	if ($theme -notin @('light', 'dark')) {
-		throw "Invalid theme. Please use 'light' or 'dark'."
-	}
-
-	# Return the theme
-	return @{
-		'light' = @{
-			Command                  = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Strawberry']))m";
-			Comment                  = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m";
-			ContinuationPrompt       = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m";
-			Default                  = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m"; # DefaultTokenColor
-			Emphasis                 = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m";
-			Error                    = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m";
-			InlinePrediction         = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m";
-			Keyword                  = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m";
-			ListPrediction           = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m";
-			Member                   = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m";
-			Number                   = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m";
-			Operator                 = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m";
-			Parameter                = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m";
-			String                   = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m";
-			Type                     = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m";
-			Variable                 = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m";
-			ListPredictionSelected   = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m";
-			Selection                = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m";
-		};
-		'dark' = @{
-			Command                  = $PSStyle.Foreground.FromRGB(0x0000FF);
-			Comment                  = $PSStyle.Foreground.FromRGB(0x006400);
-			ContinuationPrompt       = $PSStyle.Foreground.FromRGB(0x0000FF);
-			Default                  = $PSStyle.Foreground.FromRGB(0x0000FF); # DefaultTokenColor
-			Emphasis                 = $PSStyle.Foreground.FromRGB(0x287BF0);
-			Error                    = $PSStyle.Foreground.FromRGB(0xE50000);
-			InlinePrediction         = $PSStyle.Foreground.FromRGB(0x93A1A1);
-			Keyword                  = $PSStyle.Foreground.FromRGB(0x00008b);
-			ListPrediction           = $PSStyle.Foreground.FromRGB(0x06DE00);
-			Member                   = $PSStyle.Foreground.FromRGB(0x000000);
-			Number                   = $PSStyle.Foreground.FromRGB(0x800080);
-			Operator                 = $PSStyle.Foreground.FromRGB(0x757575);
-			Parameter                = $PSStyle.Foreground.FromRGB(0x000080);
-			String                   = $PSStyle.Foreground.FromRGB(0x8b0000);
-			Type                     = $PSStyle.Foreground.FromRGB(0x008080);
-			Variable                 = $PSStyle.Foreground.FromRGB(0xff4500);
-			ListPredictionSelected   = $PSStyle.Background.FromRGB(0x93A1A1);
-			Selection                = $PSStyle.Background.FromRGB(0x00BFFF)
-		}
-	}[$theme]
-}
-
 # /// Prompt configuration
 # //////////////////////////////////////////////////////////////
 
-# Prompt function
+# @brief: Prompt function
 function prompt {
 	$stat = $Global:custom_error_status -and $? # @todo
 	# Line 2 (time and status)
@@ -155,7 +117,7 @@ function prompt {
 		']',
 		'[',
 		$Global:custom_git_prompt,
-		']'		
+		']'
 	) -join ''
 	# Line 3 (empty)
 	$line_3 = ''
@@ -164,11 +126,11 @@ function prompt {
 		'|-',
 		'[',
 		(
-			("`e[48;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Ocean']));38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Snow']))m" + $Global:PD_PROMPT_USER) +
-			("`e[48;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']));38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Snow']))m" + '@') +
-			("`e[48;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Snow']));38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m" + $Global:PD_PROMPT_MACHINE) +
-			("`e[48;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']));38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Snow']))m" + ':') +
-			("`e[48;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Cayenne']));38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Snow']))m" + $(pwd)) +
+			("`e[48;2;$($Global:PD_PROMPT_COLOR['BG_USER']);38;2;$($Global:PD_PROMPT_COLOR['FG_USER'])m" + $Global:PD_PROMPT_USER) +
+			("`e[48;2;$($Global:PD_PROMPT_COLOR['BG_AUX']);38;2;$($Global:PD_PROMPT_COLOR['FG_AUX'])m" + '@') +
+			("`e[48;2;$($Global:PD_PROMPT_COLOR['BG_MACHINE']);38;2;$($Global:PD_PROMPT_COLOR['FG_MACHINE'])m" + $Global:PD_PROMPT_MACHINE) +
+			("`e[48;2;$($Global:PD_PROMPT_COLOR['BG_AUX']);38;2;$($Global:PD_PROMPT_COLOR['FG_AUX'])m" + ':') +
+			("`e[48;2;$($Global:PD_PROMPT_COLOR['BG_PATH']);38;2;$($Global:PD_PROMPT_COLOR['FG_PATH'])m" + $(pwd)) +
 			"`e[0m"
 		),
 		']',
@@ -198,14 +160,8 @@ function prompt {
 # Re-load PSReadline
 Import-Module -Name PSReadline
 
-# Set PSReadline options
-$PSReadLineOptions = @{
-	EditMode = "Emacs";
-	HistoryNoDuplicates = $true;
-	HistorySearchCursorMovesToEnd = $true;
-	Colors = Get-Theme('dark')
-}
-Set-PSReadLineOption @PSReadLineOptions
+# Set PSReadline options (@note: HistoryNoDuplicates and HistorySearchCursorMovesToEnd are of type SwitchParameter)
+Set-PSReadLineOption -EditMode Emacs -HistoryNoDuplicates -HistorySearchCursorMovesToEnd
 # Set PSReadline key handlers
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
--- conf/macos-pwsh-conf.ps1
+++ conf/macos-pwsh-conf.ps1
@@ -10,7 +10,7 @@
 
 	@date:
 	- created on 2021-08-09
-	- updated on 2024-03-20
+	- updated on 2024-11-23
 #>
 
 # /// Environment setup
@@ -19,3 +19,85 @@
 # Set environment variables
 
 Set-Item -Path "env:GPG_TTY" -Value "$(tty)" # configure GPG_TTY such that gpg-agent can find the tty for passphrase input. @note, use `Get-Command -Name tty` to check whether the command is available
+
+# Define functions
+
+# @brief: Switch the light/dark theme for the prompt
+# @details:
+# There are two major parts for the thme
+# 1. The PSReadLine coloring
+# 2. The custom prompt coloring
+# @param[in]: $Theme - the theme name
+# @param[out]: None
+# @note: This utility function is not a real function in the rigorous functional programming sense, since it has side effects.
+function Switch-Theme {
+    param (
+        [Parameter(Mandatory)]
+        [ValidateSet("Dark", "Light")]
+        [string]$Theme
+    )
+    if ($Theme -eq "Dark") {
+		$Global:PD_PROMPT_COLOR = @{
+			'BG_USER'    = (Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Ocean']);
+			'FG_USER'    = (Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Snow']);
+			'BG_MACHINE' = (Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Snow']);
+			'FG_MACHINE' = (Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']);
+			'BG_PATH'    = (Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Cayenne']);
+			'FG_PATH'    = (Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Snow']);
+			'BG_AUX'     = (Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']);
+			'FG_AUX'     = (Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Snow']);		
+		}
+		Set-PSReadLineOption -Color @{
+			'Default'                = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Snow']))m";
+			'Command'                = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Lemon']))m";
+			'Operator'               = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Lemon']))m";
+			Comment                  = $PSStyle.Foreground.FromRGB(0x006400)
+			ContinuationPrompt       = $PSStyle.Foreground.FromRGB(0x0000FF)
+			Emphasis                 = $PSStyle.Foreground.FromRGB(0x287BF0)
+			Error                    = $PSStyle.Foreground.FromRGB(0xE50000)
+			InlinePrediction         = $PSStyle.Foreground.FromRGB(0x93A1A1)
+			Keyword                  = $PSStyle.Foreground.FromRGB(0x00008b)
+			ListPrediction           = $PSStyle.Foreground.FromRGB(0x06DE00)
+			Member                   = $PSStyle.Foreground.FromRGB(0x000000)
+			Number                   = $PSStyle.Foreground.FromRGB(0x800080)
+			Parameter                = $PSStyle.Foreground.FromRGB(0x000080)
+			String                   = $PSStyle.Foreground.FromRGB(0x8b0000)
+			Type                     = $PSStyle.Foreground.FromRGB(0x008080)
+			Variable                 = $PSStyle.Foreground.FromRGB(0xff4500)
+			ListPredictionSelected   = $PSStyle.Background.FromRGB(0x93A1A1)
+			Selection                = $PSStyle.Background.FromRGB(0x00BFFF)
+		}		
+    } elseif ($Theme -eq "Light") {
+		$Global:PD_PROMPT_COLOR = @{
+			'BG_USER'    = (Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Ocean']);
+			'FG_USER'    = (Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Snow']);
+			'BG_MACHINE' = (Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']);
+			'FG_MACHINE' = (Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Snow']);
+			'BG_PATH'    = (Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Cayenne']);
+			'FG_PATH'    = (Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Snow']);
+			'BG_AUX'     = (Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Silver']);
+			'FG_AUX'     = (Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']);
+		}
+		Set-PSReadLineOption -Color @{
+			'Default'                = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Licorice']))m";
+			'Command'                = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Midnight']))m";
+			'Operator'               = "`e[38;2;$((Convert-HexColorToANSI $Global:PD_COLOR_PALLETE['Midnight']))m";
+			Comment                  = $PSStyle.Foreground.FromRGB(0x006400)
+			ContinuationPrompt       = $PSStyle.Foreground.FromRGB(0x0000FF)
+			Emphasis                 = $PSStyle.Foreground.FromRGB(0x287BF0)
+			Error                    = $PSStyle.Foreground.FromRGB(0xE50000)
+			InlinePrediction         = $PSStyle.Foreground.FromRGB(0x93A1A1)
+			Keyword                  = $PSStyle.Foreground.FromRGB(0x00008b)
+			ListPrediction           = $PSStyle.Foreground.FromRGB(0x06DE00)
+			Member                   = $PSStyle.Foreground.FromRGB(0x000000)
+			Number                   = $PSStyle.Foreground.FromRGB(0x800080)
+			Parameter                = $PSStyle.Foreground.FromRGB(0x000080)
+			String                   = $PSStyle.Foreground.FromRGB(0x8b0000)
+			Type                     = $PSStyle.Foreground.FromRGB(0x008080)
+			Variable                 = $PSStyle.Foreground.FromRGB(0xff4500)
+			ListPredictionSelected   = $PSStyle.Background.FromRGB(0x93A1A1)
+			Selection                = $PSStyle.Background.FromRGB(0x00BFFF)
+		}
+    }
+    Write-Host "Switched to $Theme theme."
+}
