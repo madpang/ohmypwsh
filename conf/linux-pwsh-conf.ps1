82eefe231940c34df67af96dd141e0d4200272c8
t-tang-rfc <156632897+t-tang-rfc@users.noreply.github.com>
2025-02-08 18:03:29 +0900

    Add open command for Linux

--- conf/linux-pwsh-conf.ps1
+++ conf/linux-pwsh-conf.ps1
@@ -1,4 +1,6 @@
 <#
+	@file: ohmypwsh/conf/linux-pwsh-conf.ps1
+
 	@brief: Linux-specific PowerShell configuration
 
 	@detaisl:
@@ -10,12 +12,13 @@
 
 	@date:
 	- created on 2021-12-20
-	- updated on 2024-03-20
+	- updated on 2025-02-03
 #>
 
-# /// Environment setup
-# ////////////////////////////////////////////////////////////
-
-# Set environment variables
+# === Environment setup
 
 Set-Item -Path "env:GPG_TTY" -Value "$(tty)" # configure GPG_TTY such that gpg-agent can find the tty for passphrase input. @note, use `Get-Command -Name tty` to check whether the command is available
+
+# === Alias configuration
+
+Set-Alias -Name "open" -Value "xdg-open" -Force
